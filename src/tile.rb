f6e6d484064efdc3cc0167475867af53b6a7290a
Johnny Panzerschrek <git@toastymofo.net>
2009-12-08 12:30:49 +0000

    refactored tile to be a Gosu::Image wrapper with position - fps boost of about 10

--- src/tile.rb
+++ src/tile.rb
@@ -1,11 +1,17 @@
-class Tile < Chingu::GameObject
-  attr_accessor :solid
+class Tile 
+  attr_accessor :solid, :x,:y,:image
   TILE_WIDTH = 16
   TILE_HEIGHT = 16
   def initialize options
-    super
+    
     @image = Gosu::Image["t_default.png"]
     @solid = true
-    
+    @x = 0
+    @y = 0
   end
+  
+  def draw
+    @image.draw(@x,@y,0)
+  end
+  
 end
